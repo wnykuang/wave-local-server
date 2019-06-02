a0180e7c0b0f6a1cb290c2db8d0d264cedecd61f
wnykuang <kuangwinnie@gmail.com>
2019-06-02 13:15:22 -0400

    update auth.js

--- server/server.js
+++ server/server.js
@@ -12,13 +12,78 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-//Users:
-app.post('/api/users/register', (req, res) =>{
-    res.status.apply(200);
+//Models
+const {User} = require('./models/user');
+
+//Middlewares
+const {auth} = require('./middleware/auth.js')
+
+
+// Users:
+app.get('/api/users/auth', auth, (req,res) => {
+    res.status(200).json({
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        role: req.user.role,
+        cart: req.user.cart,
+        history: req.user.history
+    })
 })
 
+app.post('/api/users/register', (req, res) =>{
+    const user = new User(req.body);
 
+    user.save((err, doc) => {
+        if(err) return res.json({success:false, err});
+        res.status(200).json({
+            success:true,
+            userdata: doc
+        })
+    })
+})
 
+// app.post('/api/users/register',(req,res)=>{
+//     const user = new User(req.body);
+//     user.save((err,doc)=>{
+//         if(err) return res.json({success:false,err});
+//         res.status(200).json({
+//             success: true,
+//             username: doc 
+//         })
+//     })
+// });
+
+app.post('/api/users/login', (req, res) => {
+    User.findOne({'email':req.body.email}, (err, user) => {
+        if(!user) return res.json({loginSucess:false,
+                                    message:'Auth fails, email not found'});
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch){
+                return res.json({loginSucess:false, message:"Wrong password"});
+            }
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+                res.cookie('w_auth', user.token).status(200).json({loginSucess:true}
+                );
+            })
+        })
+    })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+    User.findOneAndUpdate(
+        {_id:req.user._id},
+        {token:''},
+        (err, doc) => {
+            if(err) return res.json({success:false , err})
+            return res.status(200).send({
+                success: true
+            })
+        }
+    )
+})
 
 const port = process.env.PORT || 3002;
 
